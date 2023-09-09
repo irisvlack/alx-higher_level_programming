ed7868abbf8477269a278ad98ad49c2de3faec1f
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-09-09 12:41:18 +0300

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-
-name = dir(hidden_4)
-length = len(dir(hidden_4))
 if __name__ == "__main__":
-    for i in range(0, length):
-        if (name[i][:2] != "__"):
-            print(name[i])
+    import hidden_4
+    all_dir = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(all_dir)):
+        if avoid not in all_dir[i]:
+            print(all_dir[i])
