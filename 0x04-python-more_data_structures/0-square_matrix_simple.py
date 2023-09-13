cc120b2462effe839525b32c2e26ef67df32cc35
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-09-13 22:53:06 +0300

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
+    """
+    Function that computes the square value of all integers
+    of the matrix.
+    """
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x ** 2, i)))
+    return new_matrix
