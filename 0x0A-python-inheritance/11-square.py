e1ffebb0612809ddb6f7cbcf55e4ba22c28f01b6
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:21:10 +0300

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
+"""
+A class Square that inherits from Rectangle
+"""
 
 
 class Square(Rectangle):
+    """
+    Class Square inheriting from Rectangle.
+    Returning a string and returning the result of size * size.
+    """
     def __init__(self, size):
-        super().__init__(size, size)
         self.__size = size
+        Rectangle.__init__(self, size, size)
 
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
