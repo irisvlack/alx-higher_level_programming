1edde6431cba2398db0a03db42a9a8607c664621
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:07:40 +0300

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-def append_write(filename="", text=""):
-    with open(filename, mode="a", encoding="utf-8") as fd:
-        fd.write(text)
-    return len(text)
+"""
+Returning a Python Data Structure represented by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    json.loads: Deserializes a string to a Python object using
+    a conversion table.
+    """
+    return (json.loads(my_str))
