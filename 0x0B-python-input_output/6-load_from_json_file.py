2a342890215525f424cf3ebcc8e82185bfa714aa
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:09:32 +0300

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Creating an object from a JSON file.
+"""
 import json
 
 
-def from_json_string(my_str):
-    return json.loads(my_str)
+def load_from_json_file(filename):
+    """
+    open the file in read only and UTF-8 format.
+    json.load: Deserializes the file to a
+    Python object using a conversion table.
+    """
+    with open(filename, 'r', encoding="UTF-8") as my_file:
+        return(json.load(my_file))
