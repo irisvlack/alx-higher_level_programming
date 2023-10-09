234ec5833d42e90565e0ac39d5ef05de8096fc7f
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:20:37 +0300

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
+"""
+Class Square that inherits from Rectangle
+"""
 
 
 class Square(Rectangle):
+
+    """
+    Defining Square, initiating an instance of size, creating a method area.
+    """
+
     def __init__(self, size):
-        super().__init__(size, size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        return self.__size * self.__size
