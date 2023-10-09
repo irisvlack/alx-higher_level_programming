49f655a87bfadc645dfa29a778536538bdd4947d
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:27:16 +0300

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""
+Creating an empty class BaseGeometry
+"""
+
+
 class BaseGeometry:
+
+    """
+    empty class + public instance method area(self) + def integer_validator
+    """
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
