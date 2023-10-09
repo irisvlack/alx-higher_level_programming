daa3f4c70133fdfd1305875413cfafa9781dc71f
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:25:44 +0300

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Creating an empty class BaseGeometry
+"""
+
+
 class BaseGeometry:
+
+    """
+    empty class + public instance method area(self)
+    """
     def area(self):
         raise Exception("area() is not implemented")
