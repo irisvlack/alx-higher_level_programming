13d0808ffb1db17c399db3f285cb499aa867c609
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-10-09 23:19:00 +0300

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Inherting from a list
+"""
+
+
 class MyList(list):
+
     def print_sorted(self):
-        print(sorted(self))
+        """
+        sorting a list.
+        """
+        print (sorted(self))
