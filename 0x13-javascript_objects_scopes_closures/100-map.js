abf5f0bd23981889c0f22e0294b174251b843a86
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-12-11 19:50:43 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-let list = require('./100-data').list;
-
+const list = require('./100-data.js').list;
 console.log(list);
-
-list = list.map((n, i) => {
-  return n * i;
-});
-
-console.log(list);
+console.log(list.map((item, index) => item * index));
